3ba21c09e10af73b7e72e20de8cdae07edf50a6d
mattsluis <Sluismatthew@gmail.com>
2016-03-24 08:31:57 -0700

    still need work for 5&6

--- google_shopping.js
+++ google_shopping.js
@@ -1,5 +1,60 @@
 var data = require("./products.json")
-
 // Write your solutions below
+//Q#1
+var count = 0
+for (var i=0;i<data['items'].length;i++){
+  if (data['items'][i].kind === 'shopping#product'){
+    count++;
+   }
+}
+console.log(count + ' items');
+//
+//
+//Q#2
+//
+//
+var count = 0
+for (var i=0; i<data['items'].length; i++){
+  if (data['items'][i].product.inventories[0].availability === 'backorder'){
+    count++;
+    console.log(data.items[i].product.title);
+  }
+}
+//
+//
+//Q#3
+//
+//
+for (var i = 0; i < data['items'].length; i++){
+  if (data['items'][i].product.images.length > 1){
+    count++;
+    console.log(data.items[i].product.title);
+  }
+}
+//
+//
+//Q#4
+//c ***CASE SENSITIVITY******
+//
+var count = 0
+for (i = 0; i < data['items'].length; i++) {
+  if (data['items'][i].product.brand === "Canon") {
+    console.log(data.items[i].product.title);  
+  }
+}
+//
+//
+//Q#5 Find all items that have product author name of "eBay" and are brand "Canon".
+//5.) F
+//
+var count = 0
+ if (data)['items'][i]
 
-console.log(data["items"]);
+
+
+
+//
+//
+//Q#6 Print all the products with their brand, price, and a image link
+//6.) Print all the products with their brand, price, and a image link
+//
